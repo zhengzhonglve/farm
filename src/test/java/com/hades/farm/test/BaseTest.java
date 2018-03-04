72c6ab0495148b4336f976fb8a50b551e0a0c35d
xuzenglin <xuzeng@91xunai.com>
2018-03-04 19:04:57 +0800

    init

--- src/test/java/com/hades/farm/test/BaseTest.java
+++ src/test/java/com/hades/farm/test/BaseTest.java
@@ -5,9 +5,6 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-/**
- * Created by Ben on 16/9/8.
- */
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(Application.class)
 public class BaseTest {
